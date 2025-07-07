da2130d66d86c6ac131cc8dc928ab5c39e1a5cf5
davidesuu <davoidsmartins@gmail.com>
2025-07-07 12:17:27 -0300

    Adicionando spritesheet.py, main_sprite.json e png do personagem principal.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 import sys
+from spritesheet import Spritesheet
+
 
 #general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 800
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_icon(pygame.image.load('tomoni\images\icon.png'))
+pygame.display.set_icon(pygame.image.load('../tomoni\images\icon.png'))
 pygame.display.set_caption("TOMONI")
 running = True
 
@@ -17,7 +19,9 @@ x, y = 100, 100
 surf.fill(('blue'))
 
 
-
+#spritesheet test
+sprite_test = Spritesheet('../tomoni/images/main_sprite.png')
+link_sprite = sprite_test.get_image(21, 173 , 18, 23, 2)   
 
 
 while running:
@@ -30,6 +34,7 @@ while running:
     display_surface.fill(('darkgray'))
     x += 0.1
     display_surface.blit(surf, (x, y))
+    display_surface.blit(link_sprite, (200, 200))
     pygame.display.update()
 
 
